e9e484217e0cbd5633d244caa91c63f07dace004
Vansh <124472925+vanshwahi01@users.noreply.github.com>
2024-12-16 18:20:31 -0500

    Updated resume link in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
 
         {/* Icons Section */}
         <div className="icons-container">
-          <a href="https://drive.google.com/file/d/1eMdQolt_OC7vwxTxjrRn8cU_HUCVFzxY/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/1Yqy6amQXD2BG4bTRKDTom1xFP_qTvQjI/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFileAlt} size="2x" className="icon" />
           </a>
           <a href="https://github.com/vanshwahi01" target="_blank" rel="noopener noreferrer">
